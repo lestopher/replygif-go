7614621b42ac2235b94e303ec6d3284f3acbb78b
christopher nguyen <christoph.nguyen@gmail.com>
2014-04-19 01:01:00 -0700

    Fix struct name

--- replygif.go
+++ replygif.go
@@ -2,14 +2,14 @@ package replygif
 
 const REPLYGIF_API = "http://replygif.net/api"
 
-type ReplyGifGifData struct {
+type ReplygifGifData struct {
 	Id      string
 	Tags    []string
 	Caption []string
 	File    string
 }
 
-type ReplyGifTagData struct {
+type ReplygifTagData struct {
 	Title    string
 	Id       string
 	Reaction bool
